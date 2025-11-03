0dd77fc6d964da80effe6ef69a978b2555e11436
anahorga <ana.horga9@gmail.com>
2025-11-03 14:07:19 +0000

    Development

--- AuthenticationService/src/main/java/com/example/authenticationservice/dto/RefreshToken.java
+++
@@ -1,8 +0,0 @@
-package com.example.authenticationservice.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
-
-@Builder
-public record RefreshToken(@NotBlank String refreshToken) {
-}
